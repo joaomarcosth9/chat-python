fbb4da2d956ad9df106445102c1f9073ed1a48db
joaomarcosth9 <joaomarcosnet10@gmail.com>
2022-01-13 01:18:39 -0300

    updates

--- client_tcp.py
+++ client_tcp.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 
+# help message
 help_msg = 'Usage: python3 client_tcp.py [SERVER] [PORT]'
 
 # socket creation passing the arguments (family, type)
@@ -8,9 +9,11 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # exception handling
 try:
+    # receiving and working with command line arguments
     n = len(sys.argv)
     if n == 1 or sys.argv[1] == 'help':
         print(help_msg)
+        exit()
     if n != 3:
         raise ValueError('This programs takes exactly two arguments\n' + help_msg)
     arguments = []
@@ -21,15 +24,18 @@ try:
     while True:
         # send data to the host (encoding the string to bytes)
         msg = input('Message: ')+'\n'
+        # verify if it's a escape message
         if msg == 'quit\n' or msg == 'exit\n':
             client.send(msg.encode())
             break
         client.send(msg.encode())
         # save the response into a variable (decoding bytes to string)
         response = client.recv(1020).decode()
+        # verify if it's a escape message
         if response == 'quit\n' or response == 'exit\n':
             print('Connection closed by remote server')
             break
         print('Server says: ' + response)
 except Exception as error:
+    print(help_msg)
     print('Error:', error)
--- server_tcp.py
+++ server_tcp.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 
+# help message
 help_msg = 'Usage: python3 server_tcp.py [PORT]'
 
 # socket creation passing the arguments (family, type)
@@ -8,9 +9,11 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # exception handling
 try:
+    # receiving and working with command line arguments
     n = len(sys.argv)
     if n == 1 or sys.argv[1] == 'help':
         print(help_msg)
+        exit()
     if n != 2:
         raise ValueError('This programs takes exactly one argument\n' + help_msg)
     # set the IP and port to listen
@@ -24,17 +27,20 @@ try:
     print('Connection from: ' + client_address[0])
     while True:
         data = client_socket.recv(1024).decode()
+        # verify if it's a escape message
         if data == 'quit\n' or data == 'exit\n':
             print('Connection closed by remote client')
             break
         print('Client says: ' + data.strip('\n'))
         msg = input('Message: ')+'\n'
         print()
+        # verify if it's a escape message
         if msg == 'quit\n' or msg == 'exit\n':
             client_socket.send(msg.encode())
             break
         client_socket.send(msg.encode())
     server.close()
 except Exception as error:
+    print(help_msg)
     print('Error:', error)
     server.close()
